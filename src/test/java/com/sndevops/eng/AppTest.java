fa50e19b845e036af3ed609dab737f1dbb14210d
rohan.rasane <rohan.rasane@servicenow.com>
2020-11-19 10:02:48 -0800

    First check in

---
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -0,0 +1,24 @@
+package com.sndevops.eng;
+import org.junit.Assert;
+import org.junit.Test; 
+
+public class AppTest {
+
+    private String INPUT = "1    2 3 4 1";
+    @Test 
+    public void testLength() { 
+        Assert.assertEquals(INPUT,"1               r 6  1");
+ 
+    }
+    @Test 
+    public void testWidth() { 
+        Assert.assertEquals(INPUT,"1    2 3 4 1");
+ 
+    }
+
+    @Test 
+    public void testBreadth() { 
+        Assert.assertEquals("1","1");
+ 
+    }
+}
